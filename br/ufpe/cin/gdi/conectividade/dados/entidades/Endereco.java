b2189f841525d14b143c3425d6c337cd3cde50b6
George H. Alvares <blackbird.fly95@gmail.com>
2014-02-17 16:52:09 -0200

    Create Endereco.java

---
+++ br/ufpe/cin/gdi/conectividade/dados/entidades/Endereco.java
@@ -0,0 +1,62 @@
+package br.ufpe.cin.gdi.conectividade.dados.entidades;
+
+public class Endereco {
+	private String CEP;
+	private String logradouro;
+	private String complemento;
+	private String bairro;
+	private String cidade;
+	private String estado;
+	
+	public Endereco(String CEP, String logradouro, String complemento,
+			String bairro, String cidade, String estado) {
+		
+		this.CEP = CEP;
+		this.logradouro = logradouro;
+		this.complemento = complemento;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+}
